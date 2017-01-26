3c0621ce75d9ac4ca6df6579d9f8308cfca0b39f
zouyi6 <zouyi6@wanda.cn>
2017-01-26 10:54:57 +0800

    get user test

--- src/index.js
+++ src/index.js
@@ -1,34 +1,47 @@
 import GitHub from 'github';
 
+const github = (options) => {
+  let defaultOptions = {
+    // optional
+    protocol: "https",
+    host: "api.github.com", // should be api.github.com for GitHub
+    headers: {
+      "user-agent": "PHP-CPM's GitHub-Rank Spider" // GitHub is happy with a unique user agent
+    },
+    Promise: Promise,
+    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
+    timeout: 5000
+  }
+  let runOptions = Object.assign({}, defaultOptions, options)
+  
+  return new GitHub(runOptions);
+}
+const searchUser = async(options) => {
+
+// unauthenticated client
+  
+  try {
+    
+    let data = await github({
+      debug: true,
+    }).search.users(options)
+    if (data && data.items && data.total_count) {
+      return data
+    }
+  } catch (e) {
+    console.log(e)
+    return {
+      items: [],
+      total_count: 0,
+    }
+  }
+}
 const job = async() => {
 
 // unauthenticated client
-  const github = new GitHub(
-    {
-      // optional
-      debug: true,
-      protocol: "https",
-      host: "api.github.com", // should be api.github.com for GitHub
-      pathPrefix: "/api/v3", // for some GHEs; none for GitHub
-      headers: {
-        "user-agent": "PHP-CPM's GitHub-Rank Spider" // GitHub is happy with a unique user agent
-      },
-      Promise: Promise,
-      followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
-      timeout: 5000
-    }
-  );
-  let data ;
-  try {
-  
-    data = await github.search.users({
-      q: 'followers:>1000'
-    })
-  } catch (e) {
-    console.log(e)
-  }
-  
-  console.log(data)
+  searchUser({
+    q: 'followers:>1000'
+  })
   
 }
 
